b8de50a5e91c9243a0c6ec4adb67c03b99a5d48f
AliClark2023 <1102533@uad.ac.uk>
2026-02-02 15:13:50 +0000

    Added direction struct for perlin worm step function

--- Source/HonorsTesting/HeaderFiles/MapStructs.h
+++ Source/HonorsTesting/HeaderFiles/MapStructs.h
@@ -5,6 +5,26 @@
 #include "GameplayTagContainer.h"
 #include "MapStructs.generated.h"
 
+// describing neighbouring tiles
+UENUM(BlueprintType)
+enum class ETileNeighbour : uint8{
+	North UMETA(DisplayName = "North"),
+	Northeast UMETA(DisplayName = "NorthEast"),
+	Southeast UMETA(DisplayName = "SouthEast"),
+	South UMETA(DisplayName = "South"),
+	Southwest UMETA(DisplayName = "SouthWest"),
+	Northwest UMETA(DisplayName = "NorthWest")
+};
+// describing region types
+UENUM(BlueprintType)
+enum class ERegionType : uint8{
+	Lava UMETA(DisplayName = "Lava"),
+	Water UMETA(DisplayName = "Water"),
+	Moss UMETA(DisplayName = "Moss"),
+	Ice UMETA(DisplayName = "Ice"),
+	Rock UMETA(DisplayName = "Rock"),
+};
+
 // describes tile properties
 USTRUCT(BlueprintType)
 struct FTilePropertiesStruct
@@ -40,22 +60,30 @@ struct FGridProperties
 	FVector EndPoint = FVector(0.0f,0.0f,0.0f);
 };
 
-// describing neighbouring tiles
-UENUM(BlueprintType)
-enum class ETileNeighbour : uint8{
-	North UMETA(DisplayName = "North"),
-	Northeast UMETA(DisplayName = "NorthEast"),
-	Southeast UMETA(DisplayName = "SouthEast"),
-	South UMETA(DisplayName = "South"),
-	Southwest UMETA(DisplayName = "SouthWest"),
-	Northwest UMETA(DisplayName = "NorthWest")
-};
-// describing region types
-UENUM(BlueprintType)
-enum class ERegionType : uint8{
-	Lava UMETA(DisplayName = "Lava"),
-	Water UMETA(DisplayName = "Water"),
-	Moss UMETA(DisplayName = "Moss"),
-	Ice UMETA(DisplayName = "Ice"),
-	Rock UMETA(DisplayName = "Rock"),
+
+// describes surrounding tiles and their corresponding angle values (degrees)
+USTRUCT(BlueprintType)
+struct FTileDirections
+{
+	GENERATED_BODY()
+	FTileDirections()
+	{
+		Directions.Add(ETileNeighbour::North,FVector2D(0, 60));
+		Directions.Add(ETileNeighbour::Northeast,FVector2D(60, 120));
+		Directions.Add(ETileNeighbour::Southeast,FVector2D(120, 180));
+		Directions.Add(ETileNeighbour::South,FVector2D(180, 240));
+		Directions.Add(ETileNeighbour::Southwest,FVector2D(240, 300));
+		Directions.Add(ETileNeighbour::Northwest,FVector2D(300, 360));
+	}
+
+	ETileNeighbour getDirection(float Angle)
+	{
+		for (auto& Dir : Directions)
+		{
+			if (Angle >= Dir.Value.X && Angle < Dir.Value.Y) return Dir.Key;
+		}
+		return ETileNeighbour::North;
+	};
+private:
+	TMap<ETileNeighbour, FVector2D> Directions;
 };
